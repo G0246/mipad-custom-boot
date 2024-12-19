06e7c9c370a0675e4866ce86fc7a0fbd9704bbbb
Veutexus <70145360+G0246@users.noreply.github.com>
2024-12-19 11:39:41 +0800

    revert: systemless

--- module/post-fs-data.sh
+++ module/post-fs-data.sh
@@ -1,27 +1,5 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
 
-# Check if path exists
-if [ -f "$MODDIR/bootpath.txt" ]; then
-    BootAnimPath=$(dirname "$(cat "$MODDIR/bootpath.txt")")
-else
-    exit 1
-fi
-
-cd "$BootAnimPath" || exit 1
-
-boot_animations=(
-    "bootanimation.zip"
-    "bootanimation01.zip"
-    "bootanimation02.zip"
-    "bootanimation03.zip"
-    "bootanimation04.zip"
-)
-
-for file in "${boot_animations[@]}"; do
-    if [ -f "$file" ]; then
-        cp -p -R "$file" "$file"
-    fi
-done
-
-exit 0
+log_message="Custom boot animations systemlessly applied from $MODDIR/system/media"
+echo "$log_message" > /cache/magisk.log
