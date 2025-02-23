ef895caeaff167730be3daad483a06791f7faf3e
Veutexus <70145360+G0246@users.noreply.github.com>
2025-02-24 02:19:44 +0800

    feat: update

--- module/customize.sh
+++ module/customize.sh
@@ -1,67 +1,90 @@
 #!/system/bin/sh
+SKIPUNZIP=0
 
-# Recovery not supported
-if [ "$BOOTMODE" != true ]; then
-    abort "! Installing from recovery is not supported. Use the Magisk app."
-fi
+key_check() {
+  while true; do
+    key_check=$(/system/bin/getevent -qlc 1)
+    key_event=$(echo "$key_check" | awk '{ print $3 }' | grep 'KEY_')
+    key_status=$(echo "$key_check" | awk '{ print $4 }')
+    if [[ "$key_event" == *"KEY_"* && "$key_status" == "DOWN" ]]; then
+      keycheck="$key_event"
+      break
+    fi
+  done
+  while true; do
+    key_check=$(/system/bin/getevent -qlc 1)
+    key_event=$(echo "$key_check" | awk '{ print $3 }' | grep 'KEY_')
+    key_status=$(echo "$key_check" | awk '{ print $4 }')
+    if [[ "$key_event" == *"KEY_"* && "$key_status" == "UP" ]]; then
+      break
+    fi
+  done
+}
+
+ui_print "*********************************************"
+ui_print "- mi-pad-custom-boot"
+ui_print "- By Veutexus (github.com/G0246)"
+ui_print "*********************************************"
 
 # Check Magisk version
 if [ "$MAGISK_VER_CODE" -lt 26100 ]; then
-    abort "! Magisk version must be 26.1 or higher. Current version: $MAGISK_VER_CODE"
+  abort "! Error: Magisk v26.1+ required!"
+fi
+
+# Check Android version
+if [[ "$API" -lt 30 ]]; then
+  abort "! Error: Android 11+ required!"
 fi
 
 # Device infos
-ui_print "- Device information:"
-ui_print "- Brand: $(getprop ro.product.brand)"
-ui_print "- Model: $(getprop ro.product.model)"
-ui_print "- Android: $(getprop ro.build.version.release)"
-ui_print "- Magisk: $MAGISK_VER"
+ui_print "- Device Information:"
+ui_print "  Brand: $(getprop ro.product.brand)"
+ui_print "  Model: $(getprop ro.product.model)"
+ui_print "  Android: $(getprop ro.build.version.release)"
+ui_print "  Magisk: $MAGISK_VER ($MAGISK_VER_CODE)"
 
-# Error on < Android 11
-if [ "$API" -lt 30 ]; then
-    abort "! Android 11 or higher is required."
-fi
+BOOTPATH="/product/media"
+BACKUP_DIR="$MODPATH/backups"
 
-ui_print "- Checking boot animation path..."
-if [ -f "/system/media/bootanimation.zip" ]; then
-    BOOTPATH="/system/media"
-elif [ -f "/system/product/media/bootanimation.zip" ]; then
-    BOOTPATH="/system/product/media"
-elif [ -f "/vendor/media/bootanimation.zip" ]; then
-    BOOTPATH="/vendor/media"
+# Create backup folder if not found
+ui_print "*********************************************"
+ui_print "- Do you want to backup your current boot animation? (Only for first time installation)"
+ui_print "  Press the following keys to proceed:"
+ui_print "  Volume + to backup"
+ui_print "  Volume - to skip"
+ui_print "*********************************************"
+key_check
+if [ "$keycheck" == "KEY_VOLUMEUP" ]; then
+  if [ ! -d "$BACKUP_DIR" ]; then
+    mkdir -p "$BACKUP_DIR" && ui_print "- Created $BACKUP_DIR" || {
+      ui_print "! Unable to create backup directory at $BACKUP_DIR!"
+      abort
+    }
+  else
+    ui_print "- Using existing $BACKUP_DIR"
+  fi
+
+  ui_print "- Backing up boot animations from $BOOTPATH"
+  if [ -d "$BOOTPATH" ]; then
+    for file in "$BOOTPATH"/bootanimation*; do
+      if [ -f "$file" ]; then
+        cp -f "$file" "$BACKUP_DIR/" && {
+          ui_print "- Cloned $(basename "$file")"
+        } || {
+          ui_print "! Unable to clone $(basename "$file") to $BACKUP_DIR"
+          abort
+        }
+      fi
+    done
+  else
+    ui_print "! $BOOTPATH does not exist!"
+    abort
+  fi
 else
-    abort "! Boot animation not found. Device unsupported."
+  ui_print "- Skipping backup process."
 fi
 
-ui_print "- Boot animation found at $BOOTPATH"
-echo "$BOOTPATH" > "$MODDIR/bootpath.txt"
-
-BACKUP_DIR="/sdcard/Download/Bootanimation_backups"
-if [ ! -d "$BACKUP_DIR" ]; then
-    mkdir -p "$BACKUP_DIR"
-fi
-
-# Backup
-ui_print "- Backing up existing boot animations..."
-mkdir -p "$BACKUP_DIR"
-for file in bootanimation.zip bootanimation01.zip bootanimation02.zip bootanimation03.zip bootanimation04.zip; do
-    if [ -f "$BOOTPATH/$file" ]; then
-        cp -f "$BOOTPATH/$file" "$BACKUP_DIR/"
-        ui_print "- Backed up $file"
-    fi
-done
-
-# Replace
-SOURCE_DIR="$MODPATH/system/media"
-ui_print "- Replacing boot animations with new files..."
-for file in "$SOURCE_DIR"/*.zip; do
-    if [ -f "$file" ]; then
-        mount -o rw,remount "$(dirname "$BOOTPATH")"
-        cp -f "$file" "$BOOTPATH/"
-        ui_print "- Replaced $(basename "$file")"
-    fi
-done
-
-ui_print ""
-ui_print "- Module installation complete. Reboot to apply changes."
-ui_print "- By Veutexus (github.com/G0246)"
+ui_print "*********************************************"
+ui_print "- Installation completed!"
+ui_print "- Reboot to see new animations."
+ui_print "*********************************************"
